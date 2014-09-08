67b632ddc34e63c14dfad61e421df6e9a6896bf8
David Tschida <dmtschida1@gmail.com>
2014-09-08 14:26:50 -0400

    Typing added to number pad

--- app/src/main/java/edu/purdue/cs408/meadle/NumberPadActivity.java
+++ app/src/main/java/edu/purdue/cs408/meadle/NumberPadActivity.java
@@ -3,19 +3,45 @@ package edu.purdue.cs408.meadle;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.joanzapata.android.iconify.IconDrawable;
 import com.joanzapata.android.iconify.Iconify;
 
 
-public class NumberPadActivity extends MeadleActivity {
+public class NumberPadActivity extends MeadleActivity implements View.OnClickListener {
+
+    private static final int[] numberButtonRes = new int[] {
+            R.id.numberpad_0,
+            R.id.numberpad_1,
+            R.id.numberpad_2,
+            R.id.numberpad_3,
+            R.id.numberpad_4,
+            R.id.numberpad_5,
+            R.id.numberpad_6,
+            R.id.numberpad_7,
+            R.id.numberpad_8,
+            R.id.numberpad_9,
+    };
+
+    private EditText typeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_pad);
+
+        for(int res : numberButtonRes) {
+            findViewById(res).setOnClickListener(this);
+        }
+        findViewById(R.id.btn_delete).setOnClickListener(this);
+
+        typeView  = (EditText) findViewById(R.id.textBox);
     }
 
 
@@ -36,5 +62,57 @@ public class NumberPadActivity extends MeadleActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.numberpad_0:
+                typeCharacter('0');
+                break;
+            case R.id.numberpad_1:
+                typeCharacter('1');
+                break;
+            case R.id.numberpad_2:
+                typeCharacter('2');
+                break;
+            case R.id.numberpad_3:
+                typeCharacter('3');
+                break;
+            case R.id.numberpad_4:
+                typeCharacter('4');
+                break;
+            case R.id.numberpad_5:
+                typeCharacter('5');
+                break;
+            case R.id.numberpad_6:
+                typeCharacter('6');
+                break;
+            case R.id.numberpad_7:
+                typeCharacter('7');
+                break;
+            case R.id.numberpad_8:
+                typeCharacter('8');
+                break;
+            case R.id.numberpad_9:
+                typeCharacter('9');
+                break;
+
+            case R.id.btn_delete:
+                typeCharacter((char)8); //delete
+        }
+    }
+
+    private void typeCharacter(char c) {
+        if(c != (char)8) {
+            typeView.getText().append(c);
+        } else {
+            Log.d("TYPING", "Deleting");
+            String s = typeView.getText().toString();
+            if(s.length() > 0) {
+                s = s.substring(0, s.length() - 1);
+                typeView.setText(s);
+            }
+        }
     }
 }
