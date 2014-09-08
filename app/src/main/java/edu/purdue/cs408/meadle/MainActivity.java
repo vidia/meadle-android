e46240f2e76aee84815439cd2c75c1c2ea16e042
David Tschida <dmtschida1@gmail.com>
2014-09-08 02:13:33 -0400

    Added logo, styled action bar, removed fragments, added color def

--- app/src/main/java/edu/purdue/cs408/meadle/MainActivity.java
+++ app/src/main/java/edu/purdue/cs408/meadle/MainActivity.java
@@ -1,29 +1,17 @@
 package edu.purdue.cs408.meadle;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends MeadleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        setContentView(R.layout.activity_start);
     }
 
 
@@ -44,21 +32,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_start, container, false);
-            return rootView;
-        }
     }
 }
---
+++ app/src/main/java/edu/purdue/cs408/meadle/MeadleActivity.java
@@ -0,0 +1,26 @@
+package edu.purdue.cs408.meadle;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by david on 9/8/14.
+ */
+public class MeadleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        ActionBar actionBar = getActionBar();
+        if(actionBar != null) {
+            actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000ff")));
+            actionBar.setStackedBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000ff")));
+        }
+    }
+}
