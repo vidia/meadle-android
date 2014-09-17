744132fde70234448acd2561f5e07c299f94b40b
Kyle Potts <kyle@kptechblog.com>
2014-09-17 16:14:54 -0400

    update startMeetingTask to send data in postbody

--- app/src/main/java/edu/purdue/cs408/meadle/tasks/startMeetingTask.java
+++ app/src/main/java/edu/purdue/cs408/meadle/tasks/startMeetingTask.java
@@ -1,12 +1,20 @@
 package edu.purdue.cs408.meadle.tasks;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.text.format.Time;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import edu.purdue.cs408.meadle.interfaces.OnStartMeetingFinishedListener;
 
@@ -16,18 +24,44 @@ import edu.purdue.cs408.meadle.interfaces.OnStartMeetingFinishedListener;
 public class startMeetingTask extends AsyncTask<Void,Void,Void>{
     public static String BASEURL = "http://meadle.herokuapp.com/";
     private OnStartMeetingFinishedListener listener ;
+    private String userId;
+    private long lat;
+    private long lng;
+    private Context c;
 
-    public startMeetingTask(OnStartMeetingFinishedListener listener){
+    public startMeetingTask(OnStartMeetingFinishedListener listener,Context c,String userId,long lat,long lng){
         this.listener = listener;
+        this.c = c;
+        this.userId = userId;
+        this.lat = lat;
+        this.lng = lng;
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
         HttpClient client = new DefaultHttpClient();
-        HttpGet getRequest = new HttpGet(BASEURL+"/meeting");
+        HttpPost postRequest = new HttpPost(BASEURL+"/meeting");
+        JSONObject jObject = new JSONObject();
+
+        Time now = new Time();
+        now.setToNow();
+        String datetime = now.format("%d.%m.%Y %H.%M.%S");
+        StringEntity se = null;
+
+        try {
+            jObject.put("userId", userId);
+            jObject.put("lat",lat);
+            jObject.put("lng",lng);
+            jObject.put("datetime",datetime);
+            StringEntity sEnt = new StringEntity(jObject.toString());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        postRequest.setEntity(se);
         HttpResponse response = null;
         try{
-            response = client.execute(getRequest);
+            response = client.execute(postRequest);
             String jsonResp = EntityUtils.toString(response.getEntity());
 
 
