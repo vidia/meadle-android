87d4389cfa1e954a2f032c1141b8d1a3bf0eeee5
David Tschida <dmtschida1@gmail.com>
2014-09-20 19:43:22 -0400

    Added images to the yelp list

--- app/src/main/java/edu/purdue/cs408/meadle/adapters/YelpArrayAdapter.java
+++ app/src/main/java/edu/purdue/cs408/meadle/adapters/YelpArrayAdapter.java
@@ -1,9 +1,14 @@
 package edu.purdue.cs408.meadle.adapters;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.loopj.android.image.SmartImageView;
 
 import java.util.List;
 
+import edu.purdue.cs408.meadle.R;
 import edu.purdue.cs408.meadle.models.YelpLocation;
 
 /**
@@ -12,5 +17,16 @@ import edu.purdue.cs408.meadle.models.YelpLocation;
 public class YelpArrayAdapter extends StableArrayAdapter<YelpLocation> {
     public YelpArrayAdapter(Context context, int listItem, int textView, List<YelpLocation> data) {
         super(context, listItem, textView, data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = super.getView(position, convertView, parent);
+
+        SmartImageView image = (SmartImageView) convertView.findViewById(R.id.yelp_image_view);
+        if(getItem(position).image_url != null)
+            image.setImageUrl(getItem(position).image_url);
+
+        return convertView;
     }
 }
