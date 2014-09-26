c56c4bd5780f40e420476cabcc013307da7b1ae5
Kyle Potts <kyle@kptechblog.com>
2014-09-25 22:54:44 -0400

    add GPS to joinMeadle

--- app/src/main/java/edu/purdue/cs408/meadle/activities/JoinMeadleActivity.java
+++ app/src/main/java/edu/purdue/cs408/meadle/activities/JoinMeadleActivity.java
@@ -1,18 +1,35 @@
 package edu.purdue.cs408.meadle.activities;
 
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
+import edu.purdue.cs408.meadle.GcmManager;
 import edu.purdue.cs408.meadle.MeadleSharer;
+import edu.purdue.cs408.meadle.interfaces.GetGcmRegListener;
+import edu.purdue.cs408.meadle.interfaces.OnJoinMeetingFinishedListener;
+import edu.purdue.cs408.meadle.models.UserLocation;
 import edu.purdue.cs408.meadle.tasks.JoinMeetingTask;
 
 /**
  * Created by david on 9/25/14.
  */
-public class JoinMeadleActivity extends MeadleActivity {
+public class JoinMeadleActivity extends MeadleActivity implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener, GetGcmRegListener, OnJoinMeetingFinishedListener {
+    public final String TAG = "JoinMeadleActivity";
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationClient locationClient;
+    private String meadleId;
 
     @Override
     protected void onStart() {
@@ -33,7 +50,10 @@ public class JoinMeadleActivity extends MeadleActivity {
         String host = data.getHost(); // "twitter.com"
         List<String> params = data.getPathSegments();
 
-        String meadleId = params.get(0); // "status"
+         meadleId = params.get(0); // "status"
+
+        locationClient = new LocationClient(this,this,this);
+        locationClient.connect();
 
 
         //TODO: Join the meadle starting here.
@@ -42,5 +62,40 @@ public class JoinMeadleActivity extends MeadleActivity {
     public void onMeadleJoined() {
         Intent waiting = new Intent(this, WaitingActivity.class);
         startActivity(waiting);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "GPS Connected");
+        // Get the GCM RegID, wait for listener to respond
+        GcmManager gcmManager = new GcmManager(this);
+        gcmManager.getRegID(this);
+
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void OnRegIdReceived(String regId) {
+        Location l = locationClient.getLastLocation();
+        UserLocation location = new UserLocation(regId,l.getLatitude(),l.getLongitude()); //TODO: Create location.
+        JoinMeetingTask task = new JoinMeetingTask(meadleId,location,this);
+        task.execute();
+
+    }
+
+    @Override
+    public void onJoinMeetingFinished(JSONObject jsonResp) {
+        Log.d(TAG,"OnJoinMeetingFinished json=" + jsonResp.toString());
+
     }
 }
