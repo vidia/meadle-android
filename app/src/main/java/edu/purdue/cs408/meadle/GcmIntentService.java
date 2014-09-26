6ac78bbf0c96f3a1ce63d4a41fe07633503f8eae
Eric Templin <ertemplin@gmail.com>
2014-09-25 23:26:27 -0400

    Add GcmIntentService and GcmBroadcastReceiver

---
+++ app/src/main/java/edu/purdue/cs408/meadle/GcmIntentService.java
@@ -0,0 +1,65 @@
+package edu.purdue.cs408.meadle;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import edu.purdue.cs408.meadle.activities.WaitingActivity;
+
+/**
+ * Created by etemplin on 9/25/14.
+ */
+public class GcmIntentService extends IntentService {
+    public static final int NOTIFICATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received in the BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty()) { // isEmpty() has effect of unparcelling Bundle.
+            if(GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                // Post notification of received message
+                sendNotification("Received: " + extras.toString());
+                Log.i("GcmIntentService", "Received: " + extras.toString());
+            } else {
+                // Error
+                Log.e("GcmIntentService", "Received: " + extras.toString());
+            }
+        }
+        // Release the wake lock provided by the WakefulBroadcastReceiver.
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    // Put the message into a notificaiton and post if.
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, WaitingActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("GCM Notification")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                .bigText(msg))
+                .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+}
